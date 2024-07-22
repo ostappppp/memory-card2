dfd1fa1f3631069b227abe9660dcc2c0a3232e2f
Anonymous <136169348+LPopivnyak@users.noreply.github.com>
2024-07-22 16:31:37 +0300

    database

--- nazi.py
+++ nazi.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+import random
+import database
 
 app = QApplication([])
 window = QWidget()
@@ -12,29 +14,61 @@ var2_btn = QRadioButton("2")
 var3_btn = QRadioButton("3")
 var4_btn = QRadioButton("4")
 answer_btn = QPushButton("Відповісти")
+next_quest_btn = QPushButton("Наступне запитання")
+res_lbl = QLabel("Результат")
 group = QGroupBox("Варіанти відповідей")
 main_line = QVBoxLayout()
 
-horizontal_line1 = QHBoxLayout
+horizontal_line1 = QHBoxLayout()
 horizontal_line1.addWidget(menu_btn)
-horizontal_line1.addStetch(1)
+horizontal_line1.addStretch(1)
 horizontal_line1.addWidget(relax_btn)
 horizontal_line1.addWidget(timer_sbx)
-horizontal_line1.addLayout(horizontal_line1)
+main_line.addLayout(horizontal_line1)
+
+
 main_line.addWidget(quest_lbl)
 
-group_main_line  = QVBoxLayout
+group_main_line = QVBoxLayout()
 group_main_line.addWidget(var1_btn)
 group_main_line.addWidget(var2_btn)
 group_main_line.addWidget(var3_btn)
 group_main_line.addWidget(var4_btn)
+group_main_line.addWidget(res_lbl)
 group.setLayout(group_main_line)
+
 main_line.addWidget(group)
-main_line.addWidget(quest_lbl)
+main_line.addWidget(answer_btn)
+main_line.addWidget(next_quest_btn)
 
+answears = [var1_btn, var2_btn,var3_btn, var4_btn]
 
+def set_quest():
+    random.shuffle(answears)
+    current_question = database.questions[database.nomer]
+    quest_lbl.setText(current_question["запитання"])
+    answears[0].setText(current_question["Правильна відповідь"])
+    answears[1].setText(current_question["Не правильна відповідь1"])
+    answears[2].setText(current_question["Не правильна відповідь2"])
+    answears[3].setText(current_question["Не правильна відповідь3"])
+set_quest()
+res_lbl.hide()
+next_quest_btn.hide
 
+def ans_func():
+    if answears[0].isChecked():
+        res_lbl.setText("Правильно")
+    else:
+        res_lbl.setText("Не правильно")
+    answears[0].hide()
+    answears[1].hide()
+    answears[2].hide()
+    answears[3].hide()
+    res_lbl.show()
+    next_quest_btn.show()
+    answer_btn.hide()
 
+answer_btn.clicked.connect(ans_func)
 
 window.setLayout(main_line)
 window.show()
